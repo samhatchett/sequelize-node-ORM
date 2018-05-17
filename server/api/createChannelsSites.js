688a9a5a8db9c8cd67a0c726c9e32780914c5b40
Sam Hatchett <samhatchett@gmail.com>
2018-05-17 09:47:56 -0400

    update is not needed when using findOrCreate

--- server/api/createChannelsSites.js
+++ server/api/createChannelsSites.js
@@ -54,12 +54,12 @@ let updateChannelWithRow = (row,transaction) => {
   .then(([site,created]) => {
     console.log('found site: ' + site)
     let filter = _.extend(_.omit(row,'site'),{site_id:site.id}); // i hate this line.
-    return db.Channel.findOrCreate({where:filter, include:[{model:db.Site,as:'site'}], transaction})
-    .then(([channel,created]) => {
-      return channel.update(row,{transaction})
-      .then(()=>channel.setSite(site,{transaction}))
-      .then(()=>channel.save({transaction}));
-    });
+    return db.Channel.findOrCreate({where:filter, include:[{model:db.Site,as:'site'}], transaction});
+    // .then(([channel,created]) => {
+    //   return channel.update(row,{transaction})
+    //   .then(()=>channel.setSite(site,{transaction}))
+    //   .then(()=>channel.save({transaction}));
+    // });
   });
 };
 
